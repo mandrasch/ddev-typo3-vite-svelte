9f903cb007a8faa702f9aa1dc39d5cb77bca628f
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-12-08 08:22:56 +0100

    chore: install typo3 via DDEV quickstart docs

---
+++ README.md
@@ -0,0 +1,50 @@
+# ddev-typo3-vite-svelte
+
+## Local setup (first time)
+
+User: `admin`
+Password: `VwM308w5Xsuxa4C
+
+## How was this created?
+
+Followed steps of [DDEV typo3 quickstart docs](https://ddev.readthedocs.io/en/latest/users/quickstart/#typo3), but used v11:
+
+```bash
+ddev config --project-type=typo3 --docroot=public --create-docroot --php-version 8.1 && \
+	ddev start && \
+	ddev composer create "typo3/cms-base-distribution:^11" --prefer-dist && \
+	ddev exec touch public/FIRST_INSTALL && \
+	ddev launch
+
+# Finish installation in browser with database:'db',db-user:'db',host:'db'
+# Selected "Create empty starting page ""
+```
+
+NodeJS Installation:
+
+```bash
+ddev npm init -y
+ddev npm install vite @sveltejs/vite-plugin-svelte --save-dev
+```
+
+- Added scripts section to package.json
+
+- Created `vite.config.js`
+
+ddev-viteserve installation:
+
+```bash
+ddev get torenware/ddev-viteserve
+ddev restart
+
+
+Created `.ddev/env` for npm:
+
+```bash
+
+
+```
+
+- Created sitepackage via https://www.sitepackagebuilder.com/ (fluid + 11.5)
+- Installed package via composer https://docs.typo3.org/m/typo3/tutorial-sitepackage/main/en-us/ExtensionInstallation/Index.html#extension-installation-in-composer-mode
+- Used typoscript snippets for loading vite from https://github.com/fgeierst/typo3-vite-demo
---
+++ public/typo3conf/LocalConfiguration.php
@@ -0,0 +1,119 @@
+<?php
+return [
+    'BE' => [
+        'debug' => false,
+        'explicitADmode' => 'explicitAllow',
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$dW83S1Nhcjd5b2Jrck9kdA$5u5KxW29E3WNtOT73z2wWzLpGaTmxD9LLYBPHVeIn1A',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'driver' => 'mysqli',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+            'loginLogoAlt' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+    ],
+    'FE' => [
+        'debug' => false,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'GraphicsMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'RGB',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+        'processor_path_lzw' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/local/bin/mailhog sendmail test@example.org --smtp-addr 127.0.0.1:1025',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'imagesizes' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pagesection' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'encryptionKey' => '34ff30a9ec11bc75faeac55b245b025a0b611f330189e80fc0a609b75fd993b06f78a28a6a606dcf05a4706e9ee373a1',
+        'exceptionalErrors' => 4096,
+        'features' => [
+            'yamlImportsFollowDeclarationOrder' => true,
+        ],
+        'sitename' => 'Svelte Demo Site',
+        'systemMaintainers' => [
+            1,
+        ],
+    ],
+];
